a459a8eecaf82dcf0b720b4b2636c0c4c72e9d20
Brian Vargas <brian_vargas_7@hotmail.com>
2023-08-25 23:01:32 -0500

    comit python

---
+++ Practica_python_variables.py
@@ -0,0 +1,22 @@
+entero = 29
+string = 'concatenacion de variable de cada tipo'
+flotantes = 10.03
+boleano = True
+resultado = str(entero) + string + str(flotantes) + str(boleano)
+
+
+#En Python, tanto los enteros como los números de punto flotante
+#(también conocidos como flotantes) no tienen límites fijos en términos de su magnitud.
+# Sin embargo, están limitados por la cantidad de memoria disponible en tu sistema. 
+#Esto significa que puedes trabajar con enteros y flotantes de gran tamaño hasta que te quedes sin memoria en tu computadora.
+
+
+n = 0 + 2 + 4 + 6 + 8
+r_suma = n
+
+#resultado de la operacion entre las variables de tipo primitivo
+print(resultado)
+#resultado suma primeros numeros pares
+print(n)
+#variable en la que guardo el resultado de la suma
+print(r_suma)
--- README.md
+++
@@ -1,1 +0,0 @@
-# Practica-Py1
